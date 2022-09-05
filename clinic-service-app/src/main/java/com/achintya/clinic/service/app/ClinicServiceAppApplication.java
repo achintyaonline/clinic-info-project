f60a16875476995ff77a23e100673a12a744f9d2
achintyaonline <achintyaonline@gmail.com>
2022-09-05 15:12:17 +0530

    Adding Initial codebase

---
+++ clinic-service-app/src/main/java/com/achintya/clinic/service/app/ClinicServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.achintya.clinic.service.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicServiceAppApplication.class, args);
+	}
+
+}
---
+++ clinic-service-app/src/test/java/com/achintya/clinic/service/app/ClinicServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achintya.clinic.service.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
